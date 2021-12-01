ca7bda387233ea45f9d99a5fe934e2ce03ffea1a
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2021-12-02 00:14:10 +0100

    Add a page template (Single Column Template)

---
+++ page-templates/single-column.php
@@ -0,0 +1,39 @@
+<?php
+/**
+Template Name: Single Column Template
+Template Post Type: post, page
+ */
+
+get_header(); ?>
+  <div class="site-content">
+    <?php
+    while ( have_posts() ) :
+
+    the_post();
+    ?>
+
+    <article <?php post_class(); ?>>
+
+	  <?php the_post_thumbnail( 'my-custom-image-size' ); ?>
+
+      <header class="entry-header">
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+      </header><!-- .entry-header -->
+
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div><!-- .entry-content -->
+
+    </article><!-- #post-## -->
+
+    <?php
+    // If comments are open or we have at least one comment, load up the comment template.
+    if ( comments_open() || get_comments_number() ) :
+      comments_template();
+    endif;
+
+  endwhile;
+	?>
+	</div><!-- .site-content -->
+<?php
+get_footer();
